1113251ffc8d272c5171b8612531a496f184559d
Tomonaga Renta <67898532+Tomonaga-guppy@users.noreply.github.com>
2023-12-20 13:40:36 +0900

    変更なし

--- img2mov.py
+++ img2mov.py
@@ -2,7 +2,14 @@ import cv2
 import os
 import glob
 
-root_dir = r"C:\Users\zutom\BRLAB\tooth\Temporomandibular_movement\movie\scale"
+root_dir = "C:/Users/zutom/BRLAB/tooth/Temporomandibular_movement/movie/2023_12_demo"
+
+# root_dir = r"C:\Users\zutom\BRLAB\tooth\Temporomandibular_movement\movie\scale"
+
+# 画像フォルダと出力する動画の名前を指定して呼び出す
+input_dir = glob.glob(os.path.join(root_dir,"*2_n/OpenFace"))[0]
+dir_name = os.path.dirname(input_dir)
+save_path = os.path.join(dir_name,"OpenFaceFrame.mp4")
 
 def images_to_video(image_folder, video_name):
     images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
@@ -14,15 +21,11 @@ def images_to_video(image_folder, video_name):
     for image in images:
         # #frame数を動画に表示
         frame = cv2.imread(os.path.join(image_folder, image))
-        cv2.putText(frame, str(frame_count), (100, 100), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2, cv2.LINE_AA)
+        # cv2.putText(frame, str(frame_count), (100, 100), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2, cv2.LINE_AA)
         video.write(frame)
         frame_count += 1
 
     cv2.destroyAllWindows()
     video.release()
 
-# 画像フォルダと出力する動画の名前を指定して呼び出す
-input_dir = glob.glob(os.path.join(root_dir,"*d/OpenFace"))[0]
-dir_name = os.path.dirname(input_dir)
-save_path = os.path.join(dir_name,"OpenFaceFrame.mp4")
 images_to_video(input_dir, save_path)
--- mov2img.py
+++ mov2img.py
@@ -2,7 +2,16 @@ import cv2
 import os
 import glob
 
+# root_dir = "C:/Users/zutom/BRLAB/tooth/Temporomandibular_movement/movie/2023_12_demo"
 root_dir = r"C:\Users\zutom\BRLAB\tooth\Temporomandibular_movement\movie\scale"
+
+# video_path = glob.glob(os.path.join(root_dir, "*b/SealDetection.mp4"))[0]
+# dir_name = os.path.dirname(video_path)
+# output_folder = os.path.join(dir_name, "SealDetection")
+
+video_path = glob.glob(os.path.join(root_dir,"*b/OpenFace.avi"))[0]
+dir_name = os.path.dirname(video_path)
+output_folder = os.path.join(dir_name, "OpenFace")
 
 def save_frames(video_path, output_folder):
     video = cv2.VideoCapture(video_path)
@@ -14,19 +23,12 @@ def save_frames(video_path, output_folder):
         ret, frame = video.read()
         if not ret:
             break
+        # print(frame_count)
         output_path = f"{output_folder}/{str(frame_count).zfill(4)}.jpg"
         cv2.putText(frame, str(frame_count), (100, 100), cv2.FONT_HERSHEY_PLAIN, 4, (0, 255, 0), 2, cv2.LINE_AA)
         cv2.imwrite(output_path, frame)
         frame_count += 1
     video.release()
-
-video_path = glob.glob(os.path.join(root_dir, "*d/SealDetection.mp4"))[0]
-dir_name = os.path.dirname(video_path)
-output_folder = os.path.join(dir_name, "SealDetection")
-
-# video_path = glob.glob(os.path.join(root_dir,"*d/OpenFace.avi"))[0]
-# dir_name = os.path.dirname(video_path)
-# output_folder = os.path.join(dir_name, "OpenFace")
 
 if os.path.exists(output_folder) == False:
     os.mkdir(output_folder)
