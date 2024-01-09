1448793f9c414919b81eaac026635517008ec421
Tomonaga Renta <67898532+Tomonaga-guppy@users.noreply.github.com>
2024-01-09 15:19:59 +0900

    変更なし

--- tooth/3.MakeGraph.py
+++ tooth/3.MakeGraph.py
@@ -14,10 +14,11 @@ import trimesh
 root_dir = "C:/Users/zutom/BRLAB/tooth/Temporomandibular_movement/movie/2023_12_20"
 # root_dir = "C:/Users/zutom/BRLAB/tooth/Temporomandibular_movement/movie/2023_12_demo"
 
-# caliblation_time = 5 #aは2秒，d,eは4秒
+# caliblation_time = 10 #aは2秒，d,eは4秒
+corrective_condition = [[2,-5],[5,0],[5,-10],[4,-15],[4,0],[5,0]]  #a,b,c,d,e,fの順 [caliblation_time, theta_co_x]
 
-transp = False  #Trueが背景透明
-ear = False  #Trueがblink_list_npy（まばたき補正）を使用
+transp = True  #Trueが背景透明
+ear = True  #Trueがblink_list_npy（まばたき補正）を使用
 frame_draw = False  #Trueが各点にframe番号を表示
 seal_3dplot = False  #Trueがシールの3D軌道作成
 
@@ -28,10 +29,9 @@ theta_co_x = np.deg2rad(0)
 theta_co_y = np.deg2rad(0)
 theta_co_z = np.deg2rad(0)
 
-corrective_condition = [[2,-5],[5,0],[5,-10],[4,-15],[4,0],[5,0]]  #a,b,c,d,e,fの順 [caliblation_time, theta_co_x]
 
 def MakeGraph(root_dir, fps):
-    pattern = os.path.join(root_dir, '*[a-f]/landmark.npy')
+    pattern = os.path.join(root_dir, '*/landmark.npy')
     npy_files = glob.glob(pattern, recursive=True)
     num_npy_files = len(npy_files)
 
@@ -73,8 +73,8 @@ def MakeGraph(root_dir, fps):
         # df_vector_seal = pd.DataFrame(aa[:,68,1:])
         # df_vector_seal.to_csv(dir_path + "df_vector_seal.csv")
         # #df_vector_36をcsvで保存
-        # df_vector_36.to_csv(dir_path + "df_vector_36.csv")
-        # df_vector_45.to_csv(dir_path + "df_vector_45.csv")
+        df_vector_36.to_csv(dir_path + "df_vector_36.csv")
+        df_vector_45.to_csv(dir_path + "df_vector_45.csv")
 
 
         #df_vector_45のフレームとy座標をプロット
@@ -92,8 +92,8 @@ def MakeGraph(root_dir, fps):
             df_vector_45.loc[blink_frame_npy,:] = np.nan
             df_vector_45 = df_vector_45.interpolate(method='linear', limit_direction='both')
 
-            # df_vector_36.to_csv(dir_path + "df_vector_36_interpolate.csv")
-            # df_vector_45.to_csv(dir_path + "df_vector_45_interpolate.csv")
+            df_vector_36.to_csv(dir_path + "df_vector_36_interpolate.csv")
+            df_vector_45.to_csv(dir_path + "df_vector_45_interpolate.csv")
 
             # df_vector_45のフレームとy座標をプロット
             ax_45b = fig.add_subplot(1,2,2)
@@ -201,9 +201,11 @@ def MakeGraph(root_dir, fps):
                     Z.append(XX[2])
 
                 if ply_create == True:
+                    # print(f"{frame_number} ply_create == True")
+
                     # # if frame_number == 122 or frame_number == 240 or frame_number == 161:  #a1最大開口時
                     # if frame_number == 150 or frame_number == 514:  #b1
-                    # if frame_number == 150:
+                    if frame_number%30==0 or frame_number == 457:
                         if os.path.isfile(dir_path + f"plycam/random_cloud{frame_number}.ply"):
                             random_ply_path = dir_path + f"plycam/random_cloud{frame_number}.ply"
                             print(random_ply_path)
@@ -312,6 +314,7 @@ def MakeGraph(root_dir, fps):
         # print(f"zdif = {df.iloc[df['z'].idxmin()-1,2] - df.iloc[0,2]}")
 
 
+        # print(f"min_y_sg_index = {df_sg['y'].idxmin()}")  #df['y']が最小を取る時のindexを取得
         # print(f"min_z_sg_index = {df_sg['z'].idxmin()}")  #df['z']が最小を取る時のindexを取得
         # print(f"minz_sg = {df_sg.iloc[df_sg['z'].idxmin()-1,2]}")
         # print(f"df_sg_z0 = {df_sg.iloc[0,2]}")
