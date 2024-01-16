debc43d6f27268320527674e358ac35ae9f3e260
Tomonaga Renta <67898532+Tomonaga-guppy@users.noreply.github.com>
2024-01-16 16:31:16 +0900

    側面からの撮影動画を画像で切り抜き

--- yokopix.py
+++ yokopix.py
@@ -1,8 +1,11 @@
 import cv2
 import matplotlib.pyplot as plt
 import os
+import glob
 
-root_dir = "C:/Users/zutom/BRLAB/tooth/Temporomandibular_movement/movie/2023_12_20/20231218_f"
+root_dir = "C:/Users/zutom/BRLAB/tooth/Temporomandibular_movement/movie/2023_12_20"
+dir_path = glob.glob(os.path.join(root_dir,"*b"))[0]
+print(f"dir_path = {dir_path}")
 
 def save_frames(video_path, output_dir):
     # 動画を読み込む
@@ -18,26 +21,30 @@ def save_frames(video_path, output_dir):
 
             # フレームが正常に読み込まれた場合は保存する
             if ret:
+                print(i)
                 # if i == 0 or i == 410:  # 1フレーム目と410フレーム目を保存
                 # 画像を保存する範囲を指定
-                frame = frame[600:1080,500:980]  # 1920*1080 →480*480に切り取り  fの場合
-                # frame = frame[700:1180,600:1080]  # 1920*1080 →480*480に切り取り  bの場合
+                # frame = frame[600:1080,500:980]  # 1920*1080 →480*480に切り取り  fの場合
+                frame = frame[700:1180,600:1080]  # 1920*1080 →480*480に切り取り  bの場合
                 frame_path = f"{output_dir}/"+str(i).zfill(4)+ ".jpg"
                 cv2.imwrite(frame_path, frame)
 
     # 動画を解放する
     video.release()
 # 動画のパスと保存先ディレクトリを指定する
-video_path = root_dir + "/f.mp4"
-output_dir = root_dir + "/yoko"
+video_path = dir_path + "/b.mp4"
+# video_path = dir_path + "/f.mp4"
+output_dir = dir_path + "/yoko"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
     # フレームを保存する
-    save_frames(video_path, output_dir)
+save_frames(video_path, output_dir)
 
 
-image1_path = root_dir + "/yoko/0001.jpg"
-image2_path = root_dir + "/yoko/0542.jpg"
+image1_path = output_dir + "/0001.jpg"  #b
+image2_path = output_dir + "/0405.jpg"  #
+# image1_path = root_dir + "/yoko/0001.jpg"  #f
+# image2_path = root_dir + "/yoko/0542.jpg"
 # 画像の読み込み
 image1 = cv2.imread(image1_path)
 image2 = cv2.imread(image2_path)
