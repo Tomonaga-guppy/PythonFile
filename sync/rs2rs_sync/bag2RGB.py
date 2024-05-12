5323a816204cd7ab9a0e3612d70278af06397a18
Tomonaga-guppy/Pythonfolder <7524539@ed.tus.ac.jp>
2024-05-12 18:32:04 +0900

    同期用

--- sync/rs2rs_sync/bag2RGB.py
+++ sync/rs2rs_sync/bag2RGB.py
@@ -2,63 +2,69 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 import os
+import glob
 
-root_dir = r"C:\Users\zutom\BRLAB\gait pattern\sync\test"
+# root_dir = r"C:\Users\zutom\BRLAB\gait pattern\sync\test"
+root_dir = r"C:\Users\Tomson\BRLAB\gait_pattern\sync_test\rs-rs"
 
 #input関数でターミナルに入力された番号を取得
 blinking_interval = input("点灯間隔を入力してください: ")
 
+bag_file_path_list = glob.glob(os.path.join(root_dir, f'*.bag'))
 
-# .bagファイルのパス
-bag_file_path = os.path.join(root_dir, f'output_solo_{blinking_interval}.bag')
+for bag_file_path in bag_file_path_list:
 
-# 出力ディレクトリの作成
-output_dir = os.path.join(root_dir, f'output_images_solo_{blinking_interval}')
-os.makedirs(output_dir, exist_ok=True)
+    # # .bagファイルのパスa
+    # bag_file_path = os.path.join(root_dir, f'output_solo_{blinking_interval}.bag')
 
-# パイプライン設定
-pipeline = rs.pipeline()
-config = rs.config()
+    # 出力ディレクトリの作成
+    print(f"Processing {bag_file_path}")
+    output_dir = os.path.join(root_dir, f'{os.path.basename(bag_file_path)[:-4]}')
+    os.makedirs(output_dir, exist_ok=True)
 
-# .bagファイルからのデータの読み込みを設定
-config.enable_device_from_file(bag_file_path, repeat_playback=False)
+    # パイプライン設定
+    pipeline = rs.pipeline()
+    config = rs.config()
 
-try:
-    # パイプライン開始
-    profile = pipeline.start(config)
-    playback = profile.get_device().as_playback()
-    playback.set_real_time(False)  # リアルタイム再生を無効化
+    # .bagファイルからのデータの読み込みを設定
+    config.enable_device_from_file(bag_file_path, repeat_playback=False)
 
-    # フレーム取得用のアライメントオブジェクト
-    align_to = rs.stream.color
-    align = rs.align(align_to)
+    try:
+        # パイプライン開始
+        profile = pipeline.start(config)
+        playback = profile.get_device().as_playback()
+        playback.set_real_time(False)  # リアルタイム再生を無効化
 
-    frame_count = 1  # フレーム番号を初期化
+        # フレーム取得用のアライメントオブジェクト
+        align_to = rs.stream.color
+        align = rs.align(align_to)
 
-    while True:
-        # フレームセットを待つ
-        frames = pipeline.wait_for_frames()
-        if not frames:
-            break  # フレームがもうない場合はループを抜ける
+        frame_count = 1  # フレーム番号を初期化
 
-        # アライメント処理
-        aligned_frames = align.process(frames)
+        while True:
+            # フレームセットを待つ
+            frames = pipeline.wait_for_frames()
+            if not frames:
+                break  # フレームがもうない場合はループを抜ける
 
-        # RGBフレームの取得
-        color_frame = aligned_frames.get_color_frame()
-        if not color_frame:
-            break
+            # アライメント処理
+            aligned_frames = align.process(frames)
 
-        # Numpy配列への変換
-        color_image = np.asanyarray(color_frame.get_data())
+            # RGBフレームの取得
+            color_frame = aligned_frames.get_color_frame()
+            if not color_frame:
+                break
 
-        # ファイル名を連番で生成し、画像を保存
-        frame_id = f'{frame_count:04d}'  # 4桁のファイル番号
-        output_filename = os.path.join(output_dir, f'image_{frame_id}.jpg')
-        cv2.imwrite(output_filename, color_image)
-        frame_count += 1
+            # Numpy配列への変換
+            color_image = np.asanyarray(color_frame.get_data())
 
-finally:
-    # パイプライン停止
-    pipeline.stop()
-    print(f"Saved {frame_count - 1} images to {output_dir}")
+            # ファイル名を連番で生成し、画像を保存
+            frame_id = f'{frame_count:04d}'  # 4桁のファイル番号
+            output_filename = os.path.join(output_dir, f'image_{frame_id}.jpg')
+            cv2.imwrite(output_filename, color_image)
+            frame_count += 1
+
+    finally:
+        # パイプライン停止
+        pipeline.stop()
+        print(f"Saved {frame_count - 1} images to {output_dir}")
