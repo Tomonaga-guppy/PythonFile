9d9477e527d887851b7c14f3f084b4c9b22ac87e
Tomonaga Renta <67898532+Tomonaga-guppy@users.noreply.github.com>
2024-05-12 11:51:11 +0900

    bagファイルで保存するよう変更

--- sync/sync_save_bag.py
+++ sync/sync_save_bag.py
@@ -1,154 +1,42 @@
 import cv2
 import pyrealsense2 as rs
-import numpy as np
-from threading import Thread
 import os
-import serial
-import datetime
 
-
-# base_path = r"D:\Duser\Dbrlab\Desktop\tomonaga\pretest\RealSense"
 base_path = r"C:\Users\Tomson\BRLAB\Stroke\pretest\RealSense"
+base_path  = r"C:\Users\zutom\BRLAB\gait pattern\sync\test"
 
-ser = serial.Serial('COM3', 9600)  # Arduinoのポートを指定
+SERIAL_MASTER = '233722072880'
+# SERIAL_SLAVE = '231522070603'
 
-for folder in ["master_color", "master_depth", "slave_color", "slave_depth"]:
-    path = os.path.join(base_path, folder)
-    if not os.path.exists(path):
-        os.makedirs(path)
+def setup_camera(serial, file_name, sync_mode):
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_device(serial)
+    file_path = os.path.join(base_path, file_name)
+    config.enable_record_to_file(file_path)
+    config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 1280, 720, rs.format.rgb8, 30)
+    ctx = rs.context()
+    devices = ctx.query_devices()
+    device_found = False
+    for device in devices:
+        if device.get_info(rs.camera_info.serial_number) == serial:
+            device_found = True
+            sensor = device.first_depth_sensor()
+            sensor.set_option(rs.option.inter_cam_sync_mode, sync_mode)
+            pipeline.start(config)
+            break
+    if not device_found:
+        raise Exception(f"Device with serial number {serial} not found.")
+    return pipeline
 
-def save_image(image, path):
-    cv2.imwrite(path, image)
-
-# 各カメラのパイプラインを初期化
-master_pipeline = rs.pipeline()
-slave_pipeline = rs.pipeline()
-master_config = rs.config()
-slave_config = rs.config()
-
-# 使用するデバイスのシリアル番号を指定(check_serial_num.pyで1台ずつ確認できます)
-SERIAL_MASTER = '233722072880'  # マスターカメラ
-SERIAL_SLAVE = '231522070603'   # スレーブカメラ
-master_config.enable_device(SERIAL_MASTER)
-slave_config.enable_device(SERIAL_SLAVE)
-
-# 深度ストリームの設定
-master_config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
-slave_config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
-# カラーストリームの設定
-master_config.enable_stream(rs.stream.color, 1280, 720, rs.format.rgb8, 30)
-slave_config.enable_stream(rs.stream.color, 1280, 720, rs.format.rgb8, 30)
-
-# デバイスの取得と同期モードの設定
-ctx = rs.context()
-devices = ctx.query_devices()
-master_device = None
-slave_device = None
-# 各デバイスを識別
-for dev in devices:
-    if dev.get_info(rs.camera_info.serial_number) == SERIAL_MASTER:
-        master_device = dev
-    elif dev.get_info(rs.camera_info.serial_number) == SERIAL_SLAVE:
-        slave_device = dev
-
-if not master_device or not slave_device:
-    raise Exception("指定されたシリアル番号のデバイスが見つかりませんでした。")
-
-# 同期モードの設定
-master_sensor = master_device.first_depth_sensor()
-slave_sensor = slave_device.first_depth_sensor()
-master_sensor.set_option(rs.option.inter_cam_sync_mode, 1)  # マスターとして設定
-slave_sensor.set_option(rs.option.inter_cam_sync_mode, 2)   # スレーブとして設定
-
-# カメラの起動
-master_pipeline.start(master_config)
-slave_pipeline.start(slave_config)
-
-def process_and_display_image(master_color_image, master_depth_image, slave_color_image, slave_depth_image, frame_counter):
-    # 画像をリサイズ
-    master_color_image = cv2.resize(master_color_image, (640, 360))
-    slave_color_image = cv2.resize(slave_color_image, (640, 360))
-    master_depth_image = cv2.resize(master_depth_image, (640, 360))
-    slave_depth_image = cv2.resize(slave_depth_image, (640, 360))
-
-    # 深度画像をカラー画像に変換
-    master_depth_image = cv2.applyColorMap(cv2.convertScaleAbs(master_depth_image, alpha=0.03), cv2.COLORMAP_JET)
-    slave_depth_image = cv2.applyColorMap(cv2.convertScaleAbs(slave_depth_image, alpha=0.03), cv2.COLORMAP_JET)
-
-    # 画像を2x2に結合
-    images = np.hstack((np.vstack((master_color_image, master_depth_image)), np.vstack((slave_color_image, slave_depth_image))))
-
-    # 画像表示
-    cv2.imshow("Images", images)
-
-def save_images(images_path):
-    for image, path in images_paths:
-        cv2.imwrite(path, image)
+master_pipeline = setup_camera(SERIAL_MASTER, 'output_master.bag', 1)
+# slave_pipeline = setup_camera(SERIAL_SLAVE, 'output_slave.bag', 2)
 
 try:
-    frame_counter = 1
-    ser.write('1'.encode())  # データをエンコードして送信 string -> bytes
     while True:
-        # フレームを取得
-        master_frames = master_pipeline.wait_for_frames()
-        slave_frames = slave_pipeline.wait_for_frames()
-
-        ser.write('1'.encode())  # データをエンコードして送信 string -> bytes
-
-        # フレームから深度画像とカラー画像を取得
-        master_depth_frame = master_frames.get_depth_frame()
-        slave_depth_frame = slave_frames.get_depth_frame()
-        master_color_frame = master_frames.get_color_frame()
-        slave_color_frame = slave_frames.get_color_frame()
-
-        if not master_depth_frame or not slave_depth_frame or not master_color_frame or not slave_color_frame:
-            continue
-
-        master_depth_image = np.asanyarray(master_depth_frame.get_data())
-        slave_depth_image = np.asanyarray(slave_depth_frame.get_data())
-        master_color_image = np.asanyarray(master_color_frame.get_data())
-        slave_color_image = np.asanyarray(slave_color_frame.get_data())
-        #画像をRGBに変換
-        master_color_image = cv2.cvtColor(master_color_image, cv2.COLOR_RGB2BGR)
-        slave_color_image = cv2.cvtColor(slave_color_image, cv2.COLOR_RGB2BGR)
-
-        images_paths = [
-            (master_depth_image, f"{base_path}/master_depth/{frame_counter}.png"),
-            (slave_depth_image, f"{base_path}/slave_depth/{frame_counter}.png"),
-            (master_color_image, f"{base_path}/master_color/{frame_counter}.png"),
-            (slave_color_image, f"{base_path}/slave_color/{frame_counter}.png")
-        ]
-
-        # スレッドを使用して画像を保存
-        Thread(target=save_images, args=(images_paths,)).start()
-
-        #4枚の画像を結合してサイズを1/4にして表示
-        master_color_image = cv2.resize(master_color_image, (640, 360))
-        slave_color_image = cv2.resize(slave_color_image, (640, 360))
-        master_depth_image = cv2.resize(master_depth_image, (640, 360))
-        slave_depth_image = cv2.resize(slave_depth_image, (640, 360))
-        #深度をカラー画像に変換
-        master_depth_image = cv2.applyColorMap(cv2.convertScaleAbs(master_depth_image, alpha=0.03), cv2.COLORMAP_JET)
-        slave_depth_image = cv2.applyColorMap(cv2.convertScaleAbs(slave_depth_image, alpha=0.03), cv2.COLORMAP_JET)
-        #画像を2*2に結合
-        images = np.hstack((np.vstack((master_color_image, master_depth_image)), np.vstack((slave_color_image, slave_depth_image))))
-        cv2.imshow("Images", images)
-
-        if frame_counter == 1:
-            imshow_time = datetime.datetime.now()
-
-
-        frame_counter += 1
-
-        # スペースキーが押されたら終了
         if cv2.waitKey(1) & 0xFF == ord(' '):
             break
-
 finally:
-    # 停止処理
     master_pipeline.stop()
-    slave_pipeline.stop()
-    cv2.destroyAllWindows()
-    #Arduinoに停止を通知してシリアルポートを閉じる
-    ser.write('2'.encode())  # データをエンコードして送信 string -> bytes
-    ser.close()
+    # slave_pipeline.stop()
