eed16b997848ba7e0552d5399c6415040cf3dbfa
Tomonaga-guppy/Pythonfolder <7524539@ed.tus.ac.jp>
2024-05-31 11:33:55 +0900

    いらないやつ消した

---
+++ props/bagtomov.py
@@ -0,0 +1,80 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import glob
+import os
+
+bagsfolder = r"C:\Users\Tomson\BRLAB\gait_pattern\first_test\recorded_data\realsense\two_dev"
+bagfiles = glob.glob(os.path.join(bagsfolder, '*device1*5*.bag'),recursive=True)
+
+
+for progress, bagfile in enumerate(bagfiles):
+    path = os.path.dirname(bagfile) + '/' + os.path.basename(bagfile).split('.')[0]
+    # print(path)
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    RGB_path = path + '/RGB_image'
+    if not os.path.exists(RGB_path):
+        os.mkdir(RGB_path)
+
+    mp4file = path + '/' + os.path.basename(bagfile).split('.')[0] + '_original.mp4'
+
+    config = rs.config()
+    config.enable_device_from_file(bagfile)
+
+    pipeline = rs.pipeline()
+    profile = pipeline.start(config)
+
+    # create Align Object
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+
+    device = profile.get_device()
+    playback = device.as_playback()
+    playback.set_real_time(False)  #リアルタイム再生をオフにするとフレームごとに処理が終わるまで待機してくれる
+
+    for stream in profile.get_streams():
+        vprof = stream.as_video_stream_profile()
+        if  vprof.format() == rs.format.rgb8:
+            frame_rate = vprof.fps()
+            size = (vprof.width(), vprof.height())
+
+    fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v') # ファイル形式(ここではmp4)
+    writer = cv2.VideoWriter(mp4file, fmt, frame_rate, size) # ライター作成
+
+    print('{}/{}: '.format(progress+1, len(bagfiles)),bagfile, "size =",size, "frame_rate =",frame_rate)
+    fps_count = 1
+
+    try:
+        pre_time = 0
+        while True:
+            frames = pipeline.wait_for_frames()
+
+            aligned_frames = align.process(frames)
+            color_frame = aligned_frames.get_color_frame()
+
+            color_image = np.asanyarray(color_frame.get_data())
+            color_image = cv2.cvtColor(color_image, cv2.COLOR_RGB2BGR)
+
+            cur_time = playback.get_position()  #再生時間の取得 単位はナノ秒
+
+            if cur_time < pre_time:  #前フレームより再生時間が進んでいなかったら終了
+                break
+
+            # if fps_count > 900:  #900フレーム以上のデータは保存しない
+            #     break
+
+            cv2.imwrite('{}/{}.png'.format(RGB_path, str(fps_count).zfill(4)), color_image)
+            writer.write(color_image)
+
+            pre_time = cur_time
+
+            print(id, "RGB", fps_count)
+            fps_count += 1
+
+    finally:
+        print(f'RGB_fps_count = {fps_count - 1}')
+        pipeline.stop()
+        writer.release()
+        cv2.destroyAllWindows()
