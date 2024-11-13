61be64a0c1a314f112463e54d5f6e854c97f8b86
Tomonaga-guppy/Pythonfolder <7524539@ed.tus.ac.jp>
2024-11-13 12:23:49 +0900

    GoProフォルダ構成ちょっと変更

--- Stroke/3D/DLT/GoPro/check_intrincis.py
+++ Stroke/3D/DLT/GoPro/check_intrincis.py
@@ -2,10 +2,11 @@ import pickle
 from pathlib import Path
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import os
+from tqdm.auto import tqdm
 
-root_dir = Path(r"G:\gait_pattern\20241111\gopro")
-intrinsics_ch_paths = list(Path(root_dir).glob("*Intrinsic*.pickle"))
+root_dir = Path(r"G:\gait_pattern\20241112\gopro")
+intrinsics_ch_paths = list(Path(root_dir).glob("*Intrinsic*front_l.pickle"))
 
 print(f"intrinsics_ch_paths: {intrinsics_ch_paths}")
 
@@ -14,24 +15,15 @@ for intrinsics_ch_path in intrinsics_ch_paths:
     with open(intrinsics_ch_path, "rb") as f:
         CameraParams = pickle.load(f)
 
-    img_path = root_dir/id/"original"/"243.png"
-    img = cv2.imread(str(img_path))
-    undistort_img = cv2.undistort(img, CameraParams['intrinsicMat'], CameraParams['distortion'])
-
-    img_resize = cv2.resize(img, (640, 360))
-    undistort_img_resize = cv2.resize(undistort_img, (640, 360))
-
-    img_resize = cv2.cvtColor(img_resize, cv2.COLOR_BGR2RGB)
-    undistort_img_resize = cv2.cvtColor(undistort_img_resize, cv2.COLOR_BGR2RGB)
-
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-    ax1.imshow(img_resize)
-    ax1.set_title("Original")
-    ax2.imshow(undistort_img_resize)
-    ax2.set_title("Undistorted")
-
-    cv2.imwrite(str(root_dir/id/"0.png"), img)
-    cv2.imwrite(str(root_dir/id/"0_undistorted.png"), undistort_img)
-    plt.savefig(root_dir/id/"0_ref.png")
-
-    plt.show()
+    img_paths = list((root_dir/id/"Intrinsic_ori").glob("*.png"))
+    save_folder = root_dir/id/"Intrinsic_undistorted"
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+    for img_path in tqdm(img_paths):
+        img = cv2.imread(str(img_path))
+        undistort_img = cv2.undistort(img, CameraParams['intrinsicMat'], CameraParams['distortion'])
+        cv2.imwrite(os.path.join(save_folder, img_path.name), undistort_img)
+    # for img_path in img_paths:
+    #     img = cv2.imread(str(img_path))
+    #     undistort_img = cv2.undistort(img, CameraParams['intrinsicMat'], CameraParams['distortion'])
+    #     cv2.imwrite(os.path.join(save_folder, img_path.name), undistort_img)
