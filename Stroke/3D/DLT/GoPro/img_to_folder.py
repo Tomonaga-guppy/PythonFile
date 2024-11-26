c27cdadd0acaacbc9ebd6d897bf3863961f90092
Tomonaga-guppy/Pythonfolder <7524539@ed.tus.ac.jp>
2024-11-26 14:06:37 +0900

    3次元再構成チェッカーボードまではクリア

---
+++ Stroke/3D/DLT/GoPro/img_to_folder.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import shutil
+
+root_dir = Path(r"C:\Users\Tomson\Pictures")
+img_paths = list(root_dir.glob("*.png"))
+
+folder_path = root_dir / "Intrinsic_fr"
+if not folder_path.exists():
+    folder_path.mkdir()
+
+# PNGファイルをint_caliフォルダに移動
+for img_path in img_paths:
+    shutil.move(str(img_path), folder_path / img_path.name)
+
+print(f"{len(img_paths)} 個のファイルを {folder_path} に移動しました。")
